ea0f45b38fe75238b701c9c69f8c91f9cc381bc8
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2023-04-04 23:33:44 +0800

    feat: first version

---
+++ src/SystemClock.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zorachka\Clock;
+
+use DateTimeImmutable;
+use Psr\Clock\ClockInterface;
+
+final class SystemClock implements ClockInterface
+{
+    public function now(): DateTimeImmutable
+    {
+        return new DateTimeImmutable();
+    }
+}
